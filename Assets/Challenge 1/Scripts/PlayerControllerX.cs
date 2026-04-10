48b71cc65c665510d6bf5803cdb8489dc241f8fb
Chum Socheata <socheatachum537@gmail.com>
2026-04-10 13:11:56 +0700

    project start

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 25.0f;
+    public float rotationSpeed = 25.0f;
+    private float verticalInput = 25.0f;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        // Get input
+        verticalInput = Input.GetAxis("Vertical");
+        
+        // Move the plane forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Tilt the plane up/down based on up/down arrow key
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+    }
+}
---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, -100);
+    }
+}
